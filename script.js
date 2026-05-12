c92000ee89b832bb7c6c517f78faa37074547c29
lazennoud-web <l.azennoud@esisa.ac.ma>
2026-05-12 12:45:25 +0100

    modification css js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("JavaScript fonctionne !");
+const btnMessage=document.getElementById("BtnMessage"); 
+const message= document.getElementById("message"); 
+
+btnMessage.addEventListener("click",function(){
+    message.textContent = "Bravo , tu as clique sur le bouton !";
+})
